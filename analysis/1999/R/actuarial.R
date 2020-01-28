faa252a15a09e26979d8341c7d5155285b5689be
Ryan Thomas <rthomas2003@gmail.com>
2020-01-27 23:30:15 -0800

    Adding estimates of development factors

--- analysis/1999/R/actuarial.R
+++ analysis/1999/R/actuarial.R
@@ -30,8 +30,14 @@ create_triangles <- function(x){
                     values_to = "value")
     
         
-    group_by(tri_data, triangle_type) %>% 
-        group_split() 
+    res <- group_by(tri_data, triangle_type) %>% 
+        group_split()
+    
+    attributes(res) <- NULL
+    
+    res_names <- map_chr(res, ~ .x$triangle_type[1])
+    
+    setNames(res, res_names)
    
 }
 
@@ -67,3 +73,45 @@ create_latest_data <- function(enriched_data, pricing_data){
                inc_paid_loss, 
                cum_paid_loss)
 }
+
+make_ldfs <- function(triangles){
+    link_ratios <- map_dfr(triangles, 
+                           ~ tibble(ldfs = list(estimate_age_to_age(.x))), 
+                           .id = "triangle_type") %>%
+        group_by(triangle_type) %>%
+        mutate(factors = list(select_age_to_age(ldfs[[1]], triangle_type)))
+    
+    link_ratios
+}
+
+estimate_age_to_age <- function(triangle) {
+    triangle %>%
+        group_by(accident_year) %>% 
+        mutate(age = 12 * (development_year + 1), next_value = lead(value)) %>% 
+        group_by(age) %>% 
+        summarize(age_to_age = estimate_factor(next_value, value)) %>%
+        fit_age_to_age()
+}
+
+fit_age_to_age <- function(link_ratios) {
+    model <- lm(log(age_to_age) ~ log(age), data = link_ratios)
+    est_values <- exp(predict(model, newdata = link_ratios[, "age"]))
+    mutate(link_ratios, est_values = est_values)
+}
+
+select_age_to_age <- function(link_ratios, triangle_type){
+    final_factors <- switch (triangle_type,
+        "inc_paid_loss" = mutate(link_ratios, select_values = est_values) %>% 
+                                select(age, select_values),
+        "open_count" = mutate(link_ratios, select_values = pmin(age_to_age, est_values)) %>%
+                                mutate(select_values = ifelse(is.na(select_values), 0.5, select_values)),
+        mutate(link_ratios, select_links = pmax(est_values, 1), select_values = cumprodrev(select_links))
+    )
+    
+    select(final_factors, age, select_values)
+}    
+cumprodrev <- function(x) {
+    tmp <- cumprod(x[length(x):1])
+    tmp[length(tmp):1]
+}
+    
--- analysis/1999/R/plan.R
+++ analysis/1999/R/plan.R
@@ -4,9 +4,9 @@ plan <- drake_plan(
     #prior_data = load_priordata(file_in("data/"))
     enriched_data = clean_enrich_data(raw_data),
     triangles = create_triangles(enriched_data),
-    latest_data = create_latest_data(enriched_data, pricing_data)
+    latest_data = create_latest_data(enriched_data, pricing_data),
     #diagnostics
-    #ldfs = make_ldfs(triangles),
+    ldfs = make_ldfs(triangles)
     #estimates = make_estimates(latest_data, ldfs),
     # report
     
